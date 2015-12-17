89096eeba29f02861534ce492520244ec3447c68
jk <jk>
2015-12-17 15:48:32 +0100

    working with Game main method

--- Game.java
+++ Game.java
@@ -15,6 +15,8 @@ public class Game
     }
 
     public static void main(String args[]) {
+        
+    Game myGame = new Game();
     
     Deck myDeck = new Deck(); //creates deck of cards
     myDeck.shuffle(); //shuffle your deck of cards
@@ -32,7 +34,9 @@ public class Game
     //scanner stuff capture the y or n
     //String input = keyboard.next().trim().toUpperCase();
     
-    boolean startGame;
+    boolean startGame = false;
+    boolean dealersTurn = false;
+    boolean newRound = false;
     
     String input = "Y";
     
@@ -45,21 +49,103 @@ public class Game
     
     }
     
-    while(startGame = true){
+    //nested while loop for game starts here
+    while(true) {
+    if(newRound){
+    System.out.print("Would you like to play a new round? y/n");
+    }
     
-    Card card = myDeck.getCard();
-    System.out.println( "You got: "+card );
+//     if(input.equals("Y")){
+//     
+//     }
+        
+    //is startGame a good name ?
+    while(startGame == true){
+    
+    Card playercard = myDeck.getCard();
+    System.out.println( "You got: "+playercard );
     //add points to Player-object
-    myPlayer.addScore(card.getValue());
+    myPlayer.addScore(playercard.getValue());
     System.out.println( "Your total is now: "+myPlayer.getScore());
+    
+    //check that we did not go over int scorewon
+    if(myPlayer.getScore() > myGame.scorewon){
+    System.out.println("Busted! Point to Dealer!");
+    //and also give the actual point
+    newRound = true;
+    break;
+    }
+    
+    //check if we got blackjack
+     if(myPlayer.getScore() == myGame.scorewon){
+    System.out.println("Win! Point to Player!");
+    //and also give the actual point
+    }
     
     System.out.println( "H(hit),S(stop) or Q(quit)");
     Scanner keyboard = new Scanner(System. in );
     String choice = keyboard.next().trim().toUpperCase();
-    System.out.println(choice);
+    if(choice.equals("H")){
+    startGame = true;
     
+    } else if(choice.equals("S")){
+    dealersTurn = true;
+    startGame = false;
+    break;
+    }
+    
+    else if(choice.equals("Q")){
+    startGame = false;
+    break;
+    //print out both Player and Dealer total times won
+    //check who has highest gameswon and announce the winner (or tie)!
     
     }
+    }
+    
+    if(dealersTurn == true) {
+    System.out.println("Dealers turn...");
+    }
+    
+    while(dealersTurn == true){
+    
+        Card dealercard = myDeck.getCard();
+        System.out.println( "Dealer got: "+dealercard );
+        //add points to Dealer-object
+        myDealer.addScore(dealercard.getValue());
+        System.out.println( "Dealers total is now: "+myDealer.getScore());
+        
+        //check that we did not go over int scorewon
+        if(myDealer.getScore() > myGame.scorewon){
+        System.out.println("Busted! Point to Player!");
+        //and also give the actual point
+        dealersTurn = false;
+        newRound = true;
+        break;
+        }
+        
+        //check if Dealer got blackjack
+         if(myDealer.getScore() == myGame.scorewon){
+        System.out.println("Win! Point to Dealer!");
+        //and also give the actual point
+        newRound = true;
+        }
+        
+        //Dealer will try and pick until its over the Player score or will stop at 
+        // Dealers hitlimit
+        if(myDealer.getScore() < myPlayer.getScore()){
+        dealersTurn = true;
+        } else if(myDealer.getScore() > myPlayer.getScore() && myDealer.getScore() <= myGame.scorewon ){
+        System.out.println("Dealer won!");
+        //and also give the actual point
+        dealersTurn = false;
+        newRound = true;
+        break;
+        }
+        
+    }
+    
+}
 //     myPlayer.setScore(10);
 //     int testscore = myPlayer.getScore();
 //     System.out.println(testscore);
