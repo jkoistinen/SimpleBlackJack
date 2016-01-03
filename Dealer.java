6093f34e9ae1a0be7eb7938f2e74d22af090639d
jaakko <jaakko>
2016-01-03 13:26:06 +0100

    java -jar checkstyle-6.14-all.jar -c /google_checks.xml Dealer.java

--- Dealer.java
+++ Dealer.java
@@ -1,16 +1,12 @@
-public class Dealer extends Person
-{
-    // instance variables - replace the example below with your own
-    
+public class Dealer extends Person {
+  // instance variables - replace the example below with your own
 
-    /**
-     * Constructor for objects of class Dealer
-     */
-    public Dealer()
-    {
-        // initialise instance variables
-        
-        name = "Bob";
-        hitlimit = 17;
-    }
+  /**
+  * Constructor for objects of class Dealer
+  */
+  public Dealer() {
+    // initialise instance variables
+    name = "Bob";
+    hitlimit = 17;
+  }
 }
