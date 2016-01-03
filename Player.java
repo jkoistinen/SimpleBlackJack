348dedc8a68926887fcac4e714b7f92db58e3d4c
jaakko <jaakko>
2016-01-03 13:22:57 +0100

    java -jar checkstyle-6.14-all.jar -c /google_checks.xml Player.java

--- Player.java
+++ Player.java
@@ -1,13 +1,11 @@
-public class Player extends Person
-{
-    // instance variables - replace the example below with your own
+public class Player extends Person {
+  // instance variables - replace the example below with your own
 
-    /**
-     * Constructor for objects of class Player
-     */
-    public Player()
-    {
-        // initialise instance variables
-        name = "Player";
-    }
+  /**
+  * Constructor for objects of class Player
+  */
+  public Player() {
+    // initialise instance variables
+    name = "Player";
+  }
 }
