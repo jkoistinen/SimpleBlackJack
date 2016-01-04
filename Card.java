0e5cbae65b8ad2b0d0d7b78d707c9a7aba7f437e
jaakko <jaakko>
2016-01-04 17:46:12 +0100

    checkstyle 6.14

--- Card.java
+++ Card.java
@@ -1,69 +1,64 @@
-public class Card
-{
+public class Card {
 
-    public enum Suit {
-        SPADER,
-        HJÄRTER,
-        RUTER,
-        KLÖVER;
+  public enum Suit {
+    SPADER,
+    HJÄRTER,
+    RUTER,
+    KLÖVER;
+  }
+
+  public enum Rank {
+    ESS(1),
+    TVÅ(2),
+    TRE(3),
+    FYRA(4),
+    FEM(5),
+    SEX(6),
+    SJU(7),
+    ÅTTA(8),
+    NIO(9),
+    TIO(10),
+    KNEKT(10),
+    DAM(10),
+    KUNG(10);
+
+    int value;
+
+    Rank(int v) {
+      value = v;
     }
 
-    public enum Rank {
-        ESS (1),
-
-        TVÅ (2),
-
-        TRE (3),
-
-        FYRA (4),
-
-        FEM (5),
-
-        SEX (6),
-
-        SJU (7),
-
-        ÅTTA (8),
-
-        NIO (9),
-
-        TIO (10),
-
-        KNEKT (10),
-
-        DAM (10),
-
-        KUNG (10);
-
-        int value;
-        Rank(int v) {
-            value = v;
-        }
-
-        int showValue() {
-            return value;
-        }
-
-    }
-    public final Rank rank;
-    public final Suit suit;
-
-    public Rank rank() { return rank; }
-
-    public Suit suit() { return suit; }
-
-    public String toString() { return suit +" "+ rank+" "+"("+rank.showValue()+")"; }
-
-    /**
-     * Constructor for objects of class Card
-     */
-    public Card(Rank rank, Suit suit) {
-        this.rank = rank;
-        this.suit = suit;
-    }
-    public int getValue() {
-        int value = rank.showValue();
-        return value;
+    int showValue() {
+      return value;
     }
 
+  }
+
+  public final Rank rank;
+  public final Suit suit;
+
+  public Rank rank() {
+    return rank;
+  }
+
+  public Suit suit() {
+    return suit;
+  }
+
+  public String toString() {
+    return suit + " " + rank + " " + "(" + rank.showValue() + ")";
+  }
+
+  /**
+  * Constructor for objects of class Card.
+  */
+  public Card(Rank rank, Suit suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+  
+  public int getValue() {
+    int value = rank.showValue();
+    return value;
+  }
 }
