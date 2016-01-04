c7e88d7bd067d317d8559eb91d13ce48c92d0933
jaakko <jaakko>
2016-01-04 18:23:54 +0100

    checkstyle 6.14

--- Person.java
+++ Person.java
@@ -1,53 +1,43 @@
-public class Person implements Comparable<Person>
-{
-    // instance variables
-    private int gameswon;
-    private int score; //reset every new round
+public class Person implements Comparable<Person> {
+  // instance variables
+  private int gameswon;
+  private int score; //reset every new round
 
-    public int hitlimit;
+  public int hitlimit;
+  public String name;
 
-    public String name;
+  /**
+  * Constructor for objects of class Person.
+  */
+  public Person() {
+    // initialise instance variables
+    gameswon = 0;
+    score = 0;
+    hitlimit = 15;
+  }
 
-    /**
-     * Constructor for objects of class Person
-     */
-    public Person()
-    {
-        // initialise instance variables
-        gameswon = 0;
-        score = 0;
-        hitlimit = 15;
-    }
+  public void clearScore() {
+    score = 0;
+  }
 
-    public void clearScore()
-    {
-        score = 0;
-    }
+  public void addScore(int givenscore) {
+    score = score + givenscore;
+  }
 
-    public void addScore(int givenscore)
-    {
-        score = score + givenscore;
-    }
+  public int getScore() {
+    return score;
+  }
 
-    public int getScore()
-    {
-        return score;
-    }
+  public void incrementGamesWon() {
+    gameswon = gameswon + 1;
+  }
 
-    public void incrementGamesWon()
-    {
-        gameswon = gameswon + 1;
-    }
+  public int getGamesWon() {
+    return gameswon;
+  }
 
-    public int getGamesWon()
-    {
-        return gameswon;
-    }
-
-    public int compareTo(Person other)
-    {
-        int result = getGamesWon() - other.getGamesWon();
-
-        return result;
-    }
+  public int compareTo(Person other) {
+    int result = getGamesWon() - other.getGamesWon();
+    return result;
+  }
 }
